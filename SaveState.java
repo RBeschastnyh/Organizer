bfdc09259a9b0eba56016acf6b7f3cbbab3ce2b2
RBeschastnyh <drean_page@mail.ru>
2019-03-10 02:18:17 +0300

    Добавлены контейнеры для хранения состояний кнопок, а так же механизм, не допускающий повторов при десериализации

--- SaveState.java
+++ SaveState.java
@@ -8,27 +8,45 @@ import java.util.List;
 
 public class SaveState implements Serializable {
 
-    private static volatile SaveState uniqueInstance;
-
     private List<CheckBox> list_of_duties = new ArrayList<>();
 
-    private SaveState(){
+    private List<String> list_of_names = new ArrayList<>();
+
+    private List<Boolean> list_of_states = new ArrayList<>();
+
+    public SaveState(){
 
     }
 
-    public synchronized static SaveState getUniqueInstance(){
-        if(uniqueInstance == null){
-            uniqueInstance = new SaveState();
+    public void setNamesAndConditions(){
+        for(int i = 0; i < list_of_duties.size(); ++i){
+            if(i >=1 && list_of_duties.get(i).getText().equalsIgnoreCase(list_of_duties.get(i-1).getText())){
+                continue;
+            }
+            list_of_names.add(list_of_duties.get(i).getText());
+            list_of_states.add(list_of_duties.get(i).isSelected());
         }
-        return uniqueInstance;
+    }
+
+    public void setLists(List<String> l, List<Boolean> b){
+        for(int i = 0; i < l.size(); ++i){
+            CheckBox ch = new CheckBox(l.get(i));
+            ch.setSelected(b.get(i));
+            list_of_duties.add(ch);
+        }
+    }
+
+
+    public List<String> getNames(){
+        return list_of_names;
+    }
+
+    public List<Boolean> getStates(){
+        return list_of_states;
     }
 
     public void addCheckBox(CheckBox ch){
         list_of_duties.add(ch);
-    }
-
-    public void setCheckList(List<CheckBox> ls){
-        list_of_duties = ls;
     }
 
     public List<CheckBox> getCheckList(){
