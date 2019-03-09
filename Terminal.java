4cbe64e58048624be2f9391c9e4bde07ced98ca3
RBeschastnyh <drean_page@mail.ru>
2019-03-10 02:15:38 +0300

    Класс управления доступом к сохранённому состоянию savedstate

---
+++ Terminal.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Terminal {
+
+    private SaveState saveState;
+
+    public SaveState getSaveState() {
+        return saveState;
+    }
+
+    public void setSaveState(SaveState saveState) {
+        this.saveState = saveState;
+    }
+}
